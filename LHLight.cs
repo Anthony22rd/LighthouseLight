3538f2fe8c3845e7b9e2196b56124440aa2c7232
Anthony22rd <30275513+Anthony22rd@users.noreply.github.com>
2021-09-20 16:03:26 -0400

    Add files via upload

---
+++ LHLight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LHLight : MonoBehaviour
+{
+    [SerializeField] GameObject LHObject;
+    [SerializeField] float Speed = 0.1f;
+    // Update is called once per frame
+    // Rotate light house light
+    void Update()
+    {
+        LHObject.transform.Rotate(0.0f, Speed, 0.0f, Space.World);
+    }
+}
